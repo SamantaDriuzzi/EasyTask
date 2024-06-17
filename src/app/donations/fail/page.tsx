34ad6b9154cface0aa7a184ef2d09789398c927d
Delfina Merlo <137831158+D-MERLO@users.noreply.github.com>
2024-06-17 09:15:54 -0300

    css

--- src/app/donations/fail/page.tsx
+++ src/app/donations/fail/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 const DonationFailed = () => {
     return (
-        <div className="flex flex-col justify-center items-center bg-color11 h-screen pt-10 text-2xl text-black ">
+        <div className="flex flex-col justify-center items-center bg-color9 h-screen pt-10 text-2xl text-white ">
             <h2 className="text-5xl">Transacción fallida</h2>
             <Image className="p-5" src="/donations/fallida.svg" alt="gato triste" height={250} width={250} />
             <h3>Gracias por querer apoyarnos!</h3>
             <div className="mt-10">
-                <Link href="/donations" className="flex text-yellow-400 mb-3 justify-center items-center">Inténtalo nuevamente</Link>
+                <Link href="/donations" className="flex mb-3 justify-center items-center rounded-md py-1 px-3 border border-white hover:bg-color5 text-xl mt-10">Inténtalo nuevamente</Link>
             </div>
         </div>
     )
