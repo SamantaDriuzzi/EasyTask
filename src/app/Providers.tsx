5a8e6e1057af055eda09d91f1f336fcf77cd8bbf
Delfina Merlo <137831158+D-MERLO@users.noreply.github.com>
2024-06-20 04:48:45 -0300

    eliminar context de provider

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,13 +1,12 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import { AuthProvider } from "@/contextLogin/AuthContext";
-import { DonorProvider } from "@/contextDonators";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
     <AuthProvider>
-    <DonorProvider>{children}</DonorProvider>
+    {children}
     </AuthProvider>
   </SessionProvider>
   );
